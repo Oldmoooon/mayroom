5c0a97d7f9607f4fcdc6497d133f17a5a79b99c7
hujia <hujia@topjoy.com>
2019-03-23 18:54:13 +0800

    fix: 修复退出时的异常，因为db没有开启SSL加密连接。并增加退出时的钩子，使退出操作更加优雅。

--- src/main/java/name/guyue/backend/BackendApplication.java
+++ src/main/java/name/guyue/backend/BackendApplication.java
@@ -1,15 +1,61 @@
 package name.guyue.backend;
 
-import name.guyue.backend.db.DemoUserRepository;
-import name.guyue.backend.model.DemoUser;
-import org.springframework.boot.CommandLineRunner;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
 
 @SpringBootApplication public class BackendApplication {
 
     public static void main(String... args) {
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean public GracefulShutdown gracefulShutdown() {
+        return new GracefulShutdown();
+    }
+
+    @Bean ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers(gracefulShutdown());
+        return tomcat;
+    }
+
+    private class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+
+        private final Logger logger = LoggerFactory.getLogger(GracefulShutdown.class);
+        private volatile Connector connector;
+
+        @Override public void customize(Connector connector) {
+            this.connector = connector;
+        }
+
+        @Override public void onApplicationEvent(ContextClosedEvent event) {
+            logger.info("shutdown start...");
+            this.connector.pause();
+            var executor = this.connector.getProtocolHandler().getExecutor();
+            if (executor instanceof ThreadPoolExecutor) {
+                var threadPoolExecutor = (ThreadPoolExecutor) executor;
+                threadPoolExecutor.shutdown();
+                try {
+                    int timeout = 10;
+                    if (!threadPoolExecutor.awaitTermination(timeout, TimeUnit.SECONDS)) {
+                        logger.warn("try executor shutdown timeout {}.", timeout);
+                    }
+                } catch (InterruptedException e) {
+                    logger.error("shutdown executor error.", e);
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
