86d58b01bea9a950f2882e14119ee055da0d062d
hujia <hujia@topjoy.com>
2019-04-20 17:46:07 +0800

    feat: log assess url

--- src/main/java/name/guyue/backend/config/SessionConfig.java
+++ src/main/java/name/guyue/backend/config/SessionConfig.java
@@ -2,6 +2,8 @@ package name.guyue.backend.config;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -14,11 +16,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class SessionConfig implements WebMvcConfigurer {
+    private final static Logger logger = LoggerFactory.getLogger(SessionConfig.class);
     @Override public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
             //排除拦截
             .excludePathPatterns("/login")
-            .excludePathPatterns("/logout")
 
             //拦截路径
             .addPathPatterns("/**");
@@ -27,7 +29,9 @@ public class SessionConfig implements WebMvcConfigurer {
     public class LoginInterceptor implements HandlerInterceptor {
         @Override public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
             var session = request.getSession();
-            if (session.getAttribute(session.getId()) != null) {
+            Object userId = session.getAttribute(session.getId());
+            logger.info("user {} assessed: {}.", userId, request.getRequestURL());
+            if (userId != null) {
                 return true;
             }
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
